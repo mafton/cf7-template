694e1304207ebed5e3e2144612fe8ffa72462ba8
omidakhavan <top.exchanger@yahoo.com>
2015-11-22 14:15:43 -0800

    first commit

---
+++ cf7-template.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Contact Form 7 - Template
+Plugin URI: http://averta.net
+Description: Provides attractive new one template for contact form admin. Requires Contact Form 7
+Version: 1.0
+Author: averta
+Author URI: http://averta.net
+License: GPL2
+*/
+
+
+?>
